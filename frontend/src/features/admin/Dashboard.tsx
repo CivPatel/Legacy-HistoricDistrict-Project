c46197063357b80e6ccfef926f5756d69b4b7ca5
CivPatel <pshiv4255@gmail.com>
2025-09-10 15:37:54 -0500

    Dashboad added

--- frontend/src/features/admin/Dashboard.tsx
+++ frontend/src/features/admin/Dashboard.tsx
@@ -1,1 +1,1 @@
-export default function Dashboard(){return <div><h1 className='text-2xl font-semibold'>Admin Dashboard</h1><p>KPIs and quick actions.</p></div>}
+export default function Dashboard(){return <div><h1 className='text-2xl font-semibold'>Hammond Historic Distric </h1><p>KPIs and quick actions.</p></div>}
